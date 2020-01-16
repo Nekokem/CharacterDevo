a4b8ddbacb1eac8d6748de6b40e0f1affd92217b
Katie Morley <kamytherabbit@gmail.com>
2020-01-15 20:20:38 -0700

    Added ID checker

---
+++ CharacterDev/Assets/Scripts/IDChecker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IDChecker : MonoBehaviour
+{
+    public List<NameID> IDNameObj;
+    /* fill list some how??*/
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var nameIDObj = other.GetComponent<BehaviorID>().IdNameOBJ;
+
+        if (nameIDObj == null)
+            return;
+        
+        foreach (var nameId in IDNameObj)
+        {
+            if (nameId == nameIDObj)
+            {
+                print("found!");
+                break;
+            }
+        }
+    }
+}
