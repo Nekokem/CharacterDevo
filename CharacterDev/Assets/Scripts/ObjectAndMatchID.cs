673724a1fd58ff5c0e678efb796499646c984e12
Katie Morley <kamytherabbit@gmail.com>
2020-01-15 19:52:07 -0700

    Triggers

---
+++ CharacterDev/Assets/Scripts/ObjectAndMatchID.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectAndMatchID : MonoBehaviour
+{
+
+    public NameID nameIDObj;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        var otherNameID = other.GetComponent<ObjectAndMatchID>().nameIDObj;
+        if (nameIDObj == null) return;
+        var otherNameId = nameIDObj;
+        if (nameIDObj == otherNameID)
+        {
+         //do work   
+        }
+    }
+}
