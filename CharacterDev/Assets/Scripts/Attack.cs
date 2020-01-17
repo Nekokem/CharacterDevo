a991e90274d5c92f7a187a353fa304cc85f0d655
Katie Morley <kamytherabbit@gmail.com>
2020-01-17 14:08:41 -0700

    Trigger homework

---
+++ CharacterDev/Assets/Scripts/Attack.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+
+    public GameObject Player;
+    public void OnTriggerEnter(Collider other)
+    {
+        GetComponent<Health>();
+        {
+            Destroy(Player.gameObject);
+        }
+    }
+}
---
+++ CharacterDev/Assets/Scripts/Health.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float healthNum;
+
+    private void Start()
+    {
+        healthNum = 10f;
+    }
+}
