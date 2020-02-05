24708428a8c714240f0b1e406a9ed9f7bdf6a525
Katie Morley <kamytherabbit@gmail.com>
2020-02-04 17:08:17 -0700

    Rewrote script
    deleted didn't mean too...

---
+++ CharacterDev/Assets/Scripts/Practice/CharacterMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    private Vector3 position;
+
+    public int walkSpeed = 100;
+    public int jumpSpeed = 50;
+    private float gravity = 9.8f;
+
+    void Start()
+    {
+        GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        position.x = walkSpeed * Input.GetAxis("Horizontal");
+        position.z = walkSpeed * Input.GetAxis("Vertical");
+        position.y -= gravity;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            position.y = jumpSpeed;
+        }
+
+        controller.Move(position * Time.deltaTime);
+    }
+}
