39b7b733a6ea6f28e191150ec3acb38cfcfdd6c7
Katie Morley <kamytherabbit@gmail.com>
2020-02-08 19:23:30 -0700

    added usemagic script

---
+++ CharacterDev/Assets/Scripts/Practice/UseMagic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseMagic : MonoBehaviour
+{
+    public ApplyForce magic;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Instantiate(magic, transform.position, Quaternion.identity);
+
+        }
+        
+    }
+}
