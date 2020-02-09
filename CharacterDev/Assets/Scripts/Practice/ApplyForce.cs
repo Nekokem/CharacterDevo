3bcdda8e28c32a2d50ac1c559c39b43ddf04eb32
Katie Morley <kamytherabbit@gmail.com>
2020-02-08 19:23:13 -0700

    Added ApplyForce script

---
+++ CharacterDev/Assets/Scripts/Practice/ApplyForce.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class ApplyForce : MonoBehaviour
+{
+    private Rigidbody rigBody;
+    public Vector3Data forceDirection;
+    public float force = 3f;
+    
+    void Start()
+    {
+        rigBody = GetComponent<Rigidbody>();
+        rigBody.AddForce(forceDirection.value*force);
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        
+    }
+}
