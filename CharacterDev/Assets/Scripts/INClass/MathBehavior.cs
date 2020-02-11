5fbddfecc55aa137464549fcafdf67ee43e9b2fa
Katie Morley <kamytherabbit@gmail.com>
2020-02-11 14:46:12 -0700

    added mathBehavior

---
+++ CharacterDev/Assets/Scripts/INClass/MathBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+public class MathBehavior : MonoBehaviour
+{
+  public FloatData dataObj;
+  public FloatData[] floatDataList;
+  private NameID otherNameID;
+  
+  public void OnTriggerEnter(Collider other)
+  {
+
+    otherNameID = other.GetComponent<BehaviorID>().nameIDObj;
+    ChangeValue();
+
+  }
+
+  public void ChangeValue()
+  {
+    
+    foreach (var data in floatDataList)
+    {
+      if (data == otherNameID)
+      {
+        dataObj.UpdateVale(data.value);
+      }
+    }
+    
+  }
+  
+}
