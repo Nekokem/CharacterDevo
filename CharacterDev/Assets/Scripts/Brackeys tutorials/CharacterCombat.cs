370ee51451996bac8b13b2cc6dbcf78da17360b1
Katie Morley <kamytherabbit@gmail.com>
2020-02-10 18:56:12 -0700

    Added character combat script

---
+++ CharacterDev/Assets/Scripts/Brackeys tutorials/CharacterCombat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCombat : MonoBehaviour
+{
+    
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
