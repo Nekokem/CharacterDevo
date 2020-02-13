29dae823ad3a5240130f76b9f956c227db78c811
Katie Morley <kamytherabbit@gmail.com>
2020-02-13 15:58:52 -0700

    In class behavior system

---
+++ CharacterDev/Assets/MatchID/BehaviorID.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class BehaviorID : MonoBehaviour
+{
+    public NameID nameIDObj;
+}
--- CharacterDev/Assets/Scripts/BehaviorID.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BehaviorID : MonoBehaviour
-{
-    public NameID nameIDObj;
-}
