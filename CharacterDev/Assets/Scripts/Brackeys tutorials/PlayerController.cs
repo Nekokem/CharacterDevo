85b417c533d27f2d334e7eaed680619aa755f3ea
Katie Morley <kamytherabbit@gmail.com>
2020-02-13 16:00:45 -0700

    Brackeys tutorial

---
+++ CharacterDev/Assets/Scripts/Brackeys tutorials/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    private Camera cam;
+    private PlayerMotor motor;
+    
+    void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            Debug.Log("We hit it.");
+            if (Physics.Raycast(ray, out hit))
+            {
+                motor.MoveToPoint(hit.point);
+                
+                // stop focus any objs
+            }
+        }
+    }
+}
