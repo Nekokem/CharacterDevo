d86cc4567afb3c83e2e6788bc5d131c6d35fe15c
Katie Morley <kamytherabbit@gmail.com>
2020-02-13 15:59:32 -0700

    matchidbehavior

---
+++ CharacterDev/Assets/MatchID/MatchIDBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchIDBehavior : MonoBehaviour
+{
+    public List<NameID> nameIds;
+    private NameID otherIDObj;
+
+    public List<DoWorks> DoWork;
+    private void OnTriggerEnter(Collider other)
+    {
+        otherIDObj = other.GetComponent<BehaviorID>().nameIDObj;
+        CheckID();
+    }
+
+    private void CheckID()
+    {
+
+        foreach (var obj in nameIds)
+        {
+            if (obj == otherIDObj)
+            {
+                foreach (var job in DoWork)
+                {
+                    job.Work();
+                }
+            }
+        }
+        
+    }
+}
