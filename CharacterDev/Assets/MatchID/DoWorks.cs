6f07a485db5b8041788a0cb7b8cccd1c9f086141
Katie Morley <kamytherabbit@gmail.com>
2020-02-18 17:08:37 -0700

    edited work system

--- CharacterDev/Assets/MatchID/DoWorks.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-
-public abstract class DoWorks : ScriptableObject
-{
-  public abstract void Work();
-}
---
+++ CharacterDev/Assets/MatchID/WorkSystem.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+
+public abstract class WorkSystem : ScriptableObject
+{
+  public NameId NameIdObj { get; set; }
+  public UnityEvent workEvent;
+  public abstract void Work();
+}
