5e1ecfcd4a2a22fe082a89d3ae9db252ac971914
Katie Morley <kamytherabbit@gmail.com>
2020-02-18 17:08:49 -0700

    added work system manager

---
+++ CharacterDev/Assets/MatchID/WorkSystemManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class WorkSystemManager : ScriptableObject
+{
+    [Serializable]
+    public struct possibleMatches
+    {
+        public NameId nameIdObj;
+        public WorkSystem workSystemObj;
+    }
+
+    public List<possibleMatches> workIdList;
+
+}
