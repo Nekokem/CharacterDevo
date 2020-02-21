01112b75efa8652acb41e41e4f054c7f9a2a9da5
Katie Morley <kamytherabbit@gmail.com>
2020-02-20 18:25:20 -0700

    added instanceObject script

---
+++ CharacterDev/Assets/Scripts/INClass/InstanceObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking.PlayerConnection;
+
+[CreateAssetMenu]
+public class InstanceObject : ScriptableObject
+{
+    public GameObject prefab;
+    
+    public void CreateInstance(GameObject instance)
+    {
+        Instantiate(instance);
+    }
+
+    public void CreateInstanceAtLocation(Vector3Data location)
+    {
+        Instantiate(prefab, location.value, Quaternion.identity);
+    }
+}
--- CharacterDev/Assets/Scripts/ObjectAndMatchID.cs
+++ CharacterDev/Assets/Scripts/ObjectAndMatchID.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class ObjectAndMatchID : MonoBehaviour
 {
 
-    public NameID nameIDObj;
+    public NameId nameIDObj;
     
     private void OnTriggerEnter(Collider other)
     {
