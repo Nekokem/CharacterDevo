8958c06cc98a1b84aa17b571ac41bbdaaf153ffd
Katie Morley <kamytherabbit@gmail.com>
2020-02-20 22:29:48 -0700

    Added color data script

--- CharacterDev/Assets/MatchID/ColorData.cs
+++ CharacterDev/Assets/MatchID/ColorData.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
-public class NameID : ScriptableObject
+public class ColorData : NameId
 {
-    
+    public Color value = Color.blue;
 }
