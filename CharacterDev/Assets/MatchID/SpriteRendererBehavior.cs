1443183f0d6d17fe486347e462315fc86ad9aded
Katie Morley <kamytherabbit@gmail.com>
2020-02-28 18:40:02 -0700

    Added sprite renderer behavior scripts

---
+++ CharacterDev/Assets/MatchID/SpriteRendererBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteRendererBehavior : MonoBehaviour
+{
+
+    private SpriteRenderer spriteRendererObj;
+    public UnityEvent startEvent;
+    void Start()
+    {
+        spriteRendererObj = GetComponent<SpriteRenderer>();
+        startEvent.Invoke();
+    }
+
+    public void ChangeSpritecolor(ColorData colorDataObj)
+    {
+        spriteRendererObj.color = colorDataObj.value;
+    }
+    
+}
