dca8caaea5f7fb92015615f2bba2362065d12e86
Katie Morley <kamytherabbit@gmail.com>
2020-02-29 22:03:25 -0700

    Added sprite renderercontroler

---
+++ CharacterDev/Assets/MatchID/SpriteRendererControler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class SpriteRendererControler : ScriptableObject
+{
+    [HideInInspector]
+    public SpriteRenderer spriteRendererObj;
+
+    public void StoreSpriteRenderer(SpriteRenderer spriteRenderer)
+    {
+        spriteRendererObj = spriteRenderer;
+    }
+}
