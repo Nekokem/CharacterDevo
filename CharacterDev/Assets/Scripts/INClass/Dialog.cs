1bd80ffeead5ea42e23e0cd4d2f6dcd216aeb279
Katie Morley <kamytherabbit@gmail.com>
2020-03-05 21:08:28 -0700

    Added dialogue script notes

---
+++ CharacterDev/Assets/Scripts/INClass/Dialog.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialog : MonoBehaviour
+{
+
+/* have different things that need to be said
+need ui.text and then needs contain the data, then transition the data.
+need that transiton in a trigger of some sort, box colider ect.
+
+need:
+text behavior script
+triggerenter behavior script
+data
+*/
+
+/*
+ unity action job = connecting point ot objects
+ 
+ */
+
+
+}
