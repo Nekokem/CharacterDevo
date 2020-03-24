0df03e12781720fbd52d850f1ff4c672a4ba8ae5
Katie Morley <kamytherabbit@gmail.com>
2020-03-24 15:24:03 -0600

    dialoge

---
+++ CharacterDev/Assets/UI/TextBehavior.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TextBehavior : MonoBehaviour
+{
+    
+    private Text textObj;
+    public UnityEvent awakeEvent;
+    
+    void Awake()
+    {
+        textObj = GetComponent<Text>();
+    }
+    
+    public void UpdateText(StringListData stringListDataObj)
+    {
+        textObj.text = stringListDataObj.ReturnCurrentLine();
+    }
+    /*Cant use this in event system!*/
+
+    public void UpdateText(IntData intDataObj)
+    {
+        textObj.text = intDataObj.value.ToString();
+    }
+ 
+}
