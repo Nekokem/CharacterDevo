2e253106e47fe0b6f52e48cf1ff012bf7426e8f9
Katie Morley <kamytherabbit@gmail.com>
2020-03-24 15:23:26 -0600

    weapons and destoryer

---
+++ CharacterDev/Assets/Collections/Destroyer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class Destroyer : ScriptableObject
+{ 
+   public void Destroy(Object obj)
+   {
+      Destroy(obj);
+   }
+}
---
+++ CharacterDev/Assets/Collections/Weapon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class Weapon : Collectible
+{
+    public UnityEvent useEvent;
+    
+    
+    public override void Use()
+    {
+        useEvent.Invoke();
+    }
+
+    public void Attack()
+    {
+        Debug.Log("Attacked");
+    }
+
+    public void Equip()
+    {
+        
+    }
+
+    public void Upgrade()
+    {
+        
+    }
+    
+}
